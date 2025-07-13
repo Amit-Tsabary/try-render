a758d5d3a831f6835668c8661199818179e86e86
Amit Tsabary <amit.tsabary@gmail.com>
2025-07-14 01:31:22 +0300

    basic server

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to FastAPI!"}
+
+
+@app.post("/items/")
+def create_item(item: Item):
+    return {"name": item.name, "price": item.price}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
